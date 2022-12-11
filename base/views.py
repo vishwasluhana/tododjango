4d3935498ce8c204450987e64c8bce655befbee6
vishwasluhana <vishwasluhana@gmail.com>
2022-12-11 17:07:23 +0500

    add user authentication

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .models import Todo
 
 
+@login_required(login_url='login')
 def home(request):
     todos = Todo.objects.all()
     return render(request, 'index.html', {'todos': todos})
@@ -39,3 +42,21 @@ def complete_todo(request, pk):
     todo.completed = True
     todo.save()
     return redirect('home')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
